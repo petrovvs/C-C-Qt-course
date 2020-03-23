01e6d5e8d20b610fd4be71fd277e6cf598814ff9
petrovvs <kotovishche@gmail.com>
2020-03-23 21:00:46 +0300

    ...

---
+++ lessons_2/prog_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	unsigned short x = 65535;
+	printf("was x: %d \n", x);
+	x++;
+	printf("x is now: %d \n", x);
+	//printf("Hello world");
+	getchar(); //задержка окна консоли
+	return 0;
+}
+
+
+int main2()
+{
+	unsigned char x = 255;
+	printf("was x: %c \n", x);
+	x++;
+	printf("x is now: %c \n", x);
+	//printf("Hello world");
+	getchar(); //задержка окна консоли
+	return 0;
+}
+
+
+
+int main3()
+{
+	unsigned double x = 2342342523423234553342234534234^322345452345234520934509354354;
+	printf("was x: %c \n", x);
+	x++;
+	printf("x is now: %c \n", x);
+	//printf("Hello world");
+	getchar(); //задержка окна консоли
+	return 0;
+}
