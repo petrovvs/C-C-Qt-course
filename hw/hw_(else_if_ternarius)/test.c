6f8e0444d1e5cbf6fdc8c3ca8bd31de1f6dd1897
petrovvs <kotovishche@gmail.com>
2020-03-24 23:24:59 +0300

    hw

---
+++ hw/hw_(else_if_ternarius)/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+//ELSE IF
+	if(1){
+		printf("IF \n");
+	}
+	else{
+		printf("ELSE \n");
+	}
+
+// Тернарные операторы
+int a=2;
+int b=3;
+a > b ? printf("A>B \n") : printf("A<B \n");
+
+
+}
