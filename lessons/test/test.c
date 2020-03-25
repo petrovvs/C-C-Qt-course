f973314f3f7aa8245233b93f1db4d017255e9047
petrovvs <kotovishche@gmail.com>
2020-03-26 01:36:49 +0300

    ...

--- lessons/test/test.c
+++ lessons/test/test.c
@@ -2,10 +2,17 @@
 
 int main(){
 
-	int a;
-	float b;
-	char c;
-	scanf("%d %f %c", &a, &b, &c);
-	printf("%d %.3f %c\n", a, b, c);
+for (int i = 0; i < 100; i++)
+{
+	
+	printf(".");
+	sleep(1);
+	
+}
+	// int a;
+	// float b;
+	// char c;
+	// scanf("%d %f %c", &a, &b, &c);
+	// printf("%d %.3f %c\n", a, b, c);
 	
 }
